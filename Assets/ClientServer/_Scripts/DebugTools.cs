ca3c56460d073625f66f193c8c8ebb7f12d7bb01
AlmikSS <138039889+AlmikSS@users.noreply.github.com>
2025-04-23 22:39:20 +0300

    MainScripts
    Add ClientBootstrap + UnityServices Initializing
    Start write client logic

---
+++ Assets/ClientServer/_Scripts/DebugTools.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class DebugTools
+{
+    public static void ShowDebug(string text, bool enableDebugging, DebugType debugType = DebugType.Log)
+    {
+        if (!enableDebugging)
+            return;
+
+        switch (debugType)
+        {
+            case DebugType.Log:
+                Debug.Log(text);
+                break;
+            case DebugType.Warning:
+                Debug.LogWarning(text);
+                break;
+            case DebugType.Error:
+                Debug.LogError(text);
+                break;
+            default:
+                Debug.Log(text);
+                break;
+        }
+    }
+}
+
+public enum DebugType
+{
+    Log,
+    Warning,
+    Error,
+}
